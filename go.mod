dffda47ee6b530f7746aa49838d99d6bdc4157ec
JR Severn <jrsevern@moderncampus.com>
2022-11-23 15:19:35 -0800

    Adding API to get whois data on a hardcoded ipv6

--- go.mod
+++ go.mod
@@ -2,15 +2,12 @@ module github.com/cyderes
 
 go 1.19
 
-require github.com/likexian/whois v1.14.4
-
 require (
-	github.com/oschwald/maxminddb-golang v1.10.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
-	golang.org/x/sys v0.2.0 // indirect
 )
 
 require (
 	github.com/aws/aws-lambda-go v1.35.0
-	github.com/oschwald/geoip2-golang v1.8.0
+	github.com/likexian/whois v1.14.4
 )
--- main.go
+++ main.go
@@ -1,25 +1,19 @@
 package main
 
 import (
-	"log"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/likexian/whois"
 )
 
-// Handler is your Lambda function handler
-// It uses Amazon API Gateway request/responses provided by the aws-lambda-go/events package,
-// However you could use other event sources (S3, Kinesis etc), or JSON-decoded primitive types such as 'string'.
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// stdout and stderr are sent to AWS CloudWatch Logs
-	log.Printf("Processing Lambda request %t\n", request.RequestContext)
-	return events.APIGatewayProxyResponse{
-		Body:       "Hello world",
-		StatusCode: 200,
-	}, nil
+
+func main (){
+	lambda.Start(handler)
 }
 
-func main() {
-	log.Printf("Start lambda")
-	lambda.Start(Handler)
+// whois query for an ipv6
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		result, err := whois.Whois("2603:8000:1a01:3c84:f54e:4230:2378:1457")
+		ApiResponse := events.APIGatewayProxyResponse{Body: result,	StatusCode: 200}
+		return ApiResponse, err
 }
